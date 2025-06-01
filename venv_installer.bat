0b7fdcc364f2dc29a1e299e30b773772870e1f8f
Maksymilian Moskwytyn <79639894+maksblink@users.noreply.github.com>
2025-06-01 20:50:04 +0200

    fix sth

--- venv_installer.bat
+++ venv_installer.bat
@@ -6,14 +6,15 @@ git clone https://github.com/maksblink/sketchy_recogniser.git
 :: Change directory to project
 cd sketchy_recogniser || exit /b 1
 
-:: Install Poetry
-curl -sSL https://install.python-poetry.org | python
+:: Install Poetry (assumes Python is already installed)
+curl -sSL https://install.python-poetry.org/ | python
 
-:: Create a new conda environment with Python 3.12.8
-conda create --yes --name .venv python=3.12.8
+:: Create a new conda environment with Python 3.12.8 at the current directory
+set "VENV_PATH=%cd%.venv"
+conda create --yes --prefix "%VENV_PATH%" python=3.12.8
 
 :: Get the path to the new Python interpreter
-for /f "delims=" %%i in ('conda run -n .venv where python') do set PYTHON_PATH=%%i
+for /f "delims=" %%i in ('conda run --prefix "%VENV_PATH%" where python') do set "PYTHON_PATH=%%i"
 
 :: Set Poetry to use the new Python interpreter
 poetry env use "%PYTHON_PATH%"
